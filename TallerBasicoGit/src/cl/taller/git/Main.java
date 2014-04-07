191f30156d17b113589f577172a7738de1861a02
Joesanch <jsj_manic@hotmail.com>
2014-04-07 16:47:47 -0300

    Linea desde la web

--- TallerBasicoGit/src/cl/taller/git/Main.java
+++ TallerBasicoGit/src/cl/taller/git/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mi segundo commit");
+		System.out.println("Added line from the web");
 	}
 
 }
